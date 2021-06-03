84a803e8a5e1772481ec4c8834a4e63508216f8c
upasana-961 <84665075+upasana-961@users.noreply.github.com>
2021-06-03 23:04:27 +0400

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -38,6 +38,23 @@ int main(void) {
     }
   }
 }
-
+float mc_pi(int n) {
+  int circle = 0;
+  int square = 0;
+  float x, y;
+  for (int i = 1; i<=n; i++) {
+    x = frandom();
+    y = frandom();
+    if ((x*x + y*y)<=1)
+    {
+      circle++;
+    }
+    else {
+      square++;
+    }
+    square = square + circle;
+    float pi = 4*(float)circle/square;
+    return pi;
+  }
 
 
